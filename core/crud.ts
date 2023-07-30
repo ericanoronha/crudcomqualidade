58194cd4ea5e225c0726908f0991d8cbb0546370
ericanoronha <erica.noronha@live.com>
2023-07-30 01:51:29 -0300

    feat: passa formato do arquivo de js para ts e muda modo de importação de commonjs para modo es6

--- core/crud.ts
+++ core/crud.ts
@@ -1,9 +1,10 @@
-const fs = require('fs');
+//const fs = require('fs'); // CommonJS
+import fs from 'fs'; // ES6
 const DB_FILE_PATH = './core/db';
 
 console.log('[CRUD]');
 
-function create(content) {
+function create(content: string) {
   // salvar content no sistema
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
