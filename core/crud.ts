c0042d6650a42fd708abce8191720f87bf8180b7
ericanoronha <erica.noronha@live.com>
2023-08-07 23:49:40 -0300

    feat: cria tipo UUID e troca referências no código

--- core/crud.ts
+++ core/crud.ts
@@ -4,8 +4,10 @@ const DB_FILE_PATH = './core/db';
 
 console.log('[CRUD]');
 
+type UUID = string;
+
 interface Todo {
-  id: string;
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
@@ -37,7 +39,7 @@ function read(): Array<Todo> {
   return db.todos;
 }
 
-function update(id: string, partialTodo: Partial<Todo>): Todo {
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
   let updatedTodo;
   const todos = read();
   todos.forEach((currentTodo) => {
@@ -53,11 +55,11 @@ function update(id: string, partialTodo: Partial<Todo>): Todo {
   return updatedTodo;
 }
 
-function updateContentById(id: string, content: string): Todo {
+function updateContentById(id: UUID, content: string): Todo {
   return update(id, { content });
 }
 
-function deleteById(id: string) {
+function deleteById(id: UUID) {
   const all = read();
   const allExceptOne = all.filter((todo) => {
     if (id === todo.id) {
