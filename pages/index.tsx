8ed1d3faaf9c6c2679b26f7e8e745631b6f132eb
ericanoronha <erica.noronha@live.com>
2023-08-08 00:41:47 -0300

    feat: importa estilos globais na página inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { GlobalStyles } from '../src/theme/GlobalStyles';
 
 function HomePage() {
   return (
     <main>
+      <GlobalStyles />
       <header
         style={
           {
