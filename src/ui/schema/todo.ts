30b3547891705f62348f6c99ee075428b3566842
ericanoronha <erica.noronha@live.com>
2023-08-17 00:16:15 -0300

    feat: melhora validação de string no TodoSchema

--- src/ui/schema/todo.ts
+++ src/ui/schema/todo.ts
@@ -9,7 +9,7 @@ import { z as schema } from "zod";
 // }
 export const TodoSchema = schema.object({
   id: schema.string().uuid(),
-  content: schema.string(),
+  content: schema.string().nonempty(),
   date: schema.string().datetime(),
   done: schema.boolean(),
 });
