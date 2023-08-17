45e9359ba6c94ec939da938d1168ba532d5443be
ericanoronha <erica.noronha@live.com>
2023-08-17 00:26:12 -0300

    feat: cria novo handler para request toggle done

--- pages/api/todos/[id]/toggle-done.ts
+++ pages/api/todos/[id]/toggle-done.ts
@@ -4,6 +4,5 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  response.status(200).json({ message: "Tudo certo!" });
-  //console.log(request.headers);
+  response.status(200).json({ message: "Toggle done!" });
 }
