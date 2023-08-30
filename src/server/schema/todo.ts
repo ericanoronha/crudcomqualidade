1712487457ab5434c9f62b5b0ea4e6bb2bfee987
ericanoronha <erica.noronha@live.com>
2023-08-29 23:42:40 -0300

    fix: melhora validação de data

--- src/server/schema/todo.ts
+++ src/server/schema/todo.ts
@@ -3,7 +3,9 @@ import { z as schema } from "zod";
 export const TodoSchema = schema.object({
   id: schema.string().uuid(),
   content: schema.string().nonempty(),
-  date: schema.string(),
+  date: schema.string().transform((date) => {
+    return new Date(date).toISOString();
+  }),
   done: schema.boolean(),
   /*
   se done estiver como string no db, pode converter assim:
